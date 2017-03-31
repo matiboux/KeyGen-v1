775e0381efc53b06f11a79c18811b5d207fc5aee
Matiboux <matiboux@gmail.com>
2017-03-31 21:13:38 +0200

    Removed outdated links from footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 		<?php if($_GET['lang'] == 'fr') { ?>
-			<p class="tools"><a href="#<?php/*terms.php?lang=fr*/?>">Conditions d'utilisation (derni&eacute;re modification: <?php echo $lastUpdateTerms; ?>)</a></p>
-			<p class="back"><a href="http://codeit.fr/">Projet d&eacute;velop&eacute; par Matiboux (CodeIt.fr)</a></p>
+			<p class="tools"><a href="#">Conditions d'utilisation (derni&eacute;re modification: <?php echo $lastUpdateTerms; ?>)</a></p>
+			<p class="back"><a href="#">Projet d&eacute;velop&eacute; par Matiboux (CodeIt.fr)</a></p>
 		<?php } else { ?>
-			<p class="tools"><a href="#<?php/*terms.php?lang=en*/?>">Terms of Service (last update: <?php echo $lastUpdateTerms; ?>)</a></p>
-			<p class="back"><a href="http://codeit.fr/">Project developed by Matiboux (CodeIt.fr)</a></p>
+			<p class="tools"><a href="#">Terms of Service (last update: <?php echo $lastUpdateTerms; ?>)</a></p>
+			<p class="back"><a href="#">Project developed by Matiboux (CodeIt.fr)</a></p>
 		<?php } ?>
 		</div>
 	</body>
